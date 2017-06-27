8fab914b975e8edd3037a0c9fc7190a9f2f427d7
Ty Tia <TyTia77@gmail.com>
2017-06-27 17:15:13 +1000

    first

---
+++ gulpFile.js
@@ -0,0 +1,36 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const watch = require('gulp-watch');
+const autoprefixer = require('gulp-autoprefixer');
+const cssInline = require('gulp-css-inliner');
+const inlineCss = require('gulp-inline-css');
+
+gulp.task('inliner', function() {
+    return gulp.src('./*.html')
+        .pipe(inlineCss({
+            	applyStyleTags: true,
+            	applyLinkTags: true,
+            	removeStyleTags: true,
+            	removeLinkTags: true,
+                removeHtmlSelectors: true,
+                applyTableAttributes: true
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('buildcss', () => {
+    return gulp
+        .src(['style/main.scss'])
+        .pipe(sass())
+        .pipe(autoprefixer({browsers: ['last 2 versions'], cascade: false}))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch(
+        ['style/**/*.scss', 'index.html'],
+        ['buildcss', 'inliner']
+    );
+});
+
+gulp.task('default', ['watch']);
